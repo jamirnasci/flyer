a57f306036e9cb7cb49d7264af0347503dd95502
jamirnasci <jamirnasci@gmail.com>
2025-05-20 11:33:26 -0300

    vercel

--- sheet/SheetApi.js
+++ sheet/SheetApi.js
@@ -1,23 +1,19 @@
 const papa = require('papaparse')
 
-class SheetApi{
-    loadSheet(){
-        fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSi3-0T_WxjM5gmJq72CbUpY3qCH8JAMK9n4SWY4DlCdwPJk3SnpPRtSeO3QpFHY6HeiX1I1c1ZqyrQ/pub?output=csv', {
+class SheetApi {
+    async loadSheet() {
+        const res = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vSi3-0T_WxjM5gmJq72CbUpY3qCH8JAMK9n4SWY4DlCdwPJk3SnpPRtSeO3QpFHY6HeiX1I1c1ZqyrQ/pub?output=csv', {
             method: 'GET',
         })
-        .then((res)=>{
-            res.text()
-            .then((res)=>{
-                const {data, error, meta} = papa.parse(res, {header: true, dynamicTyping: true})
-                this.sheet = data
-            })
-        })   
+        const txt = await res.text()
+        const { data, error, meta } = papa.parse(txt, { header: true, dynamicTyping: true })
+        this.sheet = data
     }
-    findMotoById(id){
+    findMotoById(id) {
         let moto = this.sheet.filter(moto => moto.id == id)
         return moto
     }
-    findAll(){
+    findAll() {
         return this.sheet
     }
 }
