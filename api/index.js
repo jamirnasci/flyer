db4136730f2f5c594922c6177ceee31d756383b5
jamirnasci <jamirnasci@gmail.com>
2025-05-20 12:38:44 -0300

    vercel api2

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', IndexController.index)
 app.get('/motos', MotosController.Motos)
 app.get('/motos/:id', MotosController.Details)
-// const PORT = process.env.PORT || 3000
-// app.listen(PORT, _ => console.log(`server running on ${PORT}`))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, _ => console.log(`server running on ${PORT}`))
 
 module.exports = app
