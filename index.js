ac0a626c00d369d79fb4559cbd4e6144294ce10c
jamirnasci <jamirnasci@gmail.com>
2025-05-20 11:27:37 -0300

    vercel

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(bodyParser.urlencoded({extended: true}))
 
