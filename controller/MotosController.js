0b6ed0f9e8751b830307dbe45cdcd734790d90eb
jamirnasci <jamirnasci@gmail.com>
2025-05-20 11:59:09 -0300

    vercel

--- controller/MotosController.js
+++ controller/MotosController.js
@@ -1,17 +1,17 @@
-const { SheetApi } = require("../sheet/SheetApi");
+import { SheetApi } from "../sheet/sheetapi";
 
 
-const sheet = new SheetApii();
+const sheet = new SheetApi();
 
-exports.Motos = async (req, res) => {
+export async function Motos(req, res) {
     if (!sheet.sheet) await sheet.loadSheet(); // garante que os dados estão carregados
     const motos = sheet.findAll();
     res.render('motos', { motos, isBack: true });
-};
+}
 
-exports.Details = async (req, res) => {
+export async function Details(req, res) {
     if (!sheet.sheet) await sheet.loadSheet(); // garante que os dados estão carregados
     const id = req.params.id;
     const moto = sheet.findMotoById(id)[0];
     res.render('detalhes', { moto, isBack: true });
-};
+}
